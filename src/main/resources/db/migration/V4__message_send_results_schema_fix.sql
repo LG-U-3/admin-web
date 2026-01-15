82902f064dcfdc15d71adf7e43e569677197e208
Heyjinu_ <norang2810@naver.com>
2026-01-15 13:41:24 +0900

    feat: ANYUNGHA3-63 message_send_results V4 스키마 수정 및 생성

--- src/main/resources/db/migration/V4__message_send_results_schema_fix.sql
+++ src/main/resources/db/migration/V4__message_send_results_schema_fix.sql
@@ -1,5 +1,25 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
+-- 1) users: join_date 컬럼 삭제
+ALTER TABLE users
+  DROP COLUMN join_date;
+
+-- 2) message_send_results: 컬럼 제약(NOT NULL) 강화
 ALTER TABLE message_send_results
   MODIFY reserved_send_id BIGINT NOT NULL,
   MODIFY user_id BIGINT NOT NULL,
   MODIFY template_id BIGINT NOT NULL;
+
+ALTER TABLE message_send_results
+  MODIFY channel_id BIGINT NOT NULL;
+
+CREATE INDEX idx_msr_channel ON message_send_results(channel_id);
+
+
+ALTER TABLE message_send_results
+  ADD CONSTRAINT fk_msr_channel
+  FOREIGN KEY (channel_id) REFERENCES codes(id);
+
+
+SET FOREIGN_KEY_CHECKS = 1;
