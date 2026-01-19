d33f2bdf608d3a914e3e8bb4cf9ac1fb59a36eef
Heyjinu_ <norang2810@naver.com>
2026-01-19 16:57:43 +0900

    feat: ANYUNGHA3-71 entity templateTypeId 제거

--- src/main/java/com/example/adminweb/domain/message/MessageReservation.java
+++ src/main/java/com/example/adminweb/domain/message/MessageReservation.java
@@ -1,3 +1,22 @@
+package com.example.adminweb.domain.message;
+
+import com.example.adminweb.domain.code.Code;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "message_reservations")
 @Getter
@@ -23,6 +42,7 @@ public class MessageReservation {
     @JoinColumn(name = "template_id", nullable = false)
     private MessageTemplate template;
 
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_group_id", nullable = false)
     private com.example.adminweb.domain.user.UserGroup userGroup;
@@ -42,3 +62,5 @@ public class MessageReservation {
         this.userGroup = userGroup;
     }
 }
+
+
